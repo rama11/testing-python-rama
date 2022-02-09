1273ffb0ef1fda20be48058fcfc111be50e86352
rama11 <prof.agastyo@gmail.com>
2022-02-09 12:35:13 +0700

    add selenium on view

--- hello/views.py
+++ hello/views.py
@@ -3,10 +3,48 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+import requests
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
+
+def test(request):
+    data = [
+        "adfadf",
+        "adfadf",
+        "adfadf",
+        "adfadf",
+        "adfadf",
+        "adfadf",
+        "adfadf",
+        "adfadf",
+    ]
+    return render(request, "test.html", {"data" : data})
+
+def test_request(request):
+    r = requests.get('https://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
+
+def selenium_test(request):
+
+    driver = webdriver.Firefox()
+    driver.get("http://www.python.org")
+    assert "Python" in driver.title
+    elem = driver.find_element_by_name("q")
+    elem.clear()
+    elem.send_keys("pycon")
+    elem.send_keys(Keys.RETURN)
+    data = "success"
+    assert "No results found." not in driver.page_source
+    driver.close()
+
+    return HttpResponse(data)
 
 
 def db(request):
